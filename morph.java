71aad01477f15e65b2512cd76d0aa1616b7be954
kl275 <kl275@njit.edu>
2020-01-16 13:27:01 -0500

    Add files via upload

---
+++ morph.java
@@ -0,0 +1,17 @@
+public class morph
+{
+	string name;
+	string description;
+	stringBuilder implants;
+	int aptitudeMaximum;
+	int durability;
+	int woundThreshold;
+	int cpCost;
+	string creditCost;
+	string advantages; // this will have to be parsed
+	string morphType; // used for calculating durability
+	
+	public morph()
+	{
+	}
+}
