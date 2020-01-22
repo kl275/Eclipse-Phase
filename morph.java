203fd3be1c6910443c98cb39ede378ba106353d8
kl275 <kl275@njit.edu>
2020-01-22 12:47:33 -0500

    Update morph.java

--- morph.java
+++ morph.java
@@ -1,5 +1,15 @@
 public class morph
 {
 	string morphType;	// biomorph or synth
+	
 	// attribute bonuses -- maybe store as an array?
+	// if array value for attribute is greater than zero, player could not option it as a variable option bonus
+	// for UI, gray out the option
+	
+	// image Image
+	
+	public void morph(array, image) // constructor
+	{
+		
+	}
 }
